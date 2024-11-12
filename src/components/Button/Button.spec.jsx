53855034341073fc3b7352e3052b344c00e61b01
Luis Cesar Albuquerque Arruda <lc.albuquerque@edu.unifor.br>
2024-11-11 21:27:43 -0300

    Testing Button component
    using jest to unit test

--- src/components/Button/Button.spec.jsx
+++ src/components/Button/Button.spec.jsx
@@ -1,12 +1,36 @@
+import userEvent from "@testing-library/user-event"
 import  {Button} from "./index"
 
-const { render, screen } = require("@testing-library/react")
+const { render, screen, fireEvent } = require("@testing-library/react")
 
 describe("<Button/>", ()=>{
 
     it("should render the button with the text", ()=>{
-        render(<Button/>)
+        render(<Button text="load more"/>)
 
-        const Button = screen.getByRole()
+        const button = screen.getByRole("button",{name: /load more/i})
+
+        expect(button).toBeInTheDocument()
+    })
+
+    it("Should call function on button click", () =>{
+        const fn = jest.fn();
+
+        render(<Button text= "load more" onClick={fn}/>)
+
+        const button = screen.getByRole("button", {name: /load more/i})
+        
+        userEvent.click(button)
+
+        expect(fn).toHaveBeenCalledTimes(1)
+
+    })
+
+    it("Should be disabled when disabled is true", () =>{
+
+        //TODO
+        render(<Button text= "load more" disabled= {true}/>)
+
+        const button = screen.getByRole("button", )
     })
 })
